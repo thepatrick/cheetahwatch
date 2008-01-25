2e02d879f226f0116dc88ef175ac611d720b0ae8
patrick <patrick@9e143911-4937-0410-b81c-e45933c3531e>
2008-01-25 20:57:37 +0000

    Release v1.2.1 should be in a tag
    git-svn-id: svn+ssh://lyra/home/patrick/repo/cheetahwatch@21 9e143911-4937-0410-b81c-e45933c3531e

---
+++ tags/rel1_2_1/CWHistorySupport.m
@@ -0,0 +1,323 @@
+/* CheetahWatch, v1.2
+ * Copyright (c) 2007-2008 Patrick Quinn-Graham
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import "CWHistorySupport.h"
+
+@implementation CWHistorySupport
+
+-(void)dealloc
+{
+	if(activeConnection != nil) {
+		[activeConnection release];
+	}
+	[managedObjectContext release];
+	[coordinator release];
+	[model release];
+	[super dealloc];
+}
+-(void)setMainController:(NSObject*)cont
+{
+	mainController = cont;
+}
+//@TODO RENAME!
+-(void)iCanHasCheezburger:(NSTimer*)timer
+{
+	if(activeConnection == nil) {
+		activeConnection = [self doWeHaveAnUnclosedConnection];
+		if(activeConnection == nil) {
+			[cheezburgerWatch invalidate];
+			cheezburgerWatch = nil;
+			return;
+		}
+	}
+	NSDate *lastSeenAt = [activeConnection valueForKey:@"lastSeen"];
+	if(fabs([lastSeenAt timeIntervalSinceDate:[NSDate new]]) > 4) { // we should get updates every two seconds.
+		[self markConnectionAsClosed];
+		if(cheezburgerWatch != nil) {
+			[mainController performSelector:@selector(clearConnectionUI)];
+			[cheezburgerWatch invalidate];
+			cheezburgerWatch = nil;
+		} else {
+		}
+	}
+	
+	[managedObjectContext save:&coreDataError];
+	if(coreDataError != nil) {
+		NSLog(@"iCanHasCheezburger: save error: %@", coreDataError);
+	}
+}
+-(void)flowReportSeconds:(NSNumber*)connected withTransmitRate:(NSNumber*)transmit receiveRate:(NSNumber*)receive totalSent:(NSNumber*)sent andTotalReceived:(NSNumber*)received
+{
+	BOOL brandNew = NO;
+	
+	if(activeConnection == nil) {
+		// we need a new connection
+		[self doWeHaveAnUnclosedConnection];
+		if(activeConnection == nil) {
+			// but we didn't have one un-resumed
+			//NSLog(@"No connection exists already, so we're going to make one.");
+			[self newConnection:connected];
+			brandNew = YES;
+		} else {
+			//NSLog(@"Retrieved an unterminated connection: %@", activeConnection);
+		}
+	}// else {
+	//	NSLog(@"activeConnection wasn't nil, it was %i", [activeConnection objectID]);
+	//}
+	
+	if(!brandNew) {
+		NSNumber *lastConnected = [activeConnection valueForKey:@"connectedSeconds"];
+		if([connected compare:lastConnected] != NSOrderedDescending) {
+			//NSLog(@"flowReportSeconds: Closing connection because we've guessed a new connection has occurred.");
+			[self markConnectionAsClosed];
+			activeConnection = [self newConnection:connected];	
+		}
+	}
+	// why wrap this like this? It's easier! :)
+	if(!brandNew && firstRunSinceStartup) {
+		NSDate *lastStartTime = [activeConnection valueForKey:@"startTime"];
+		float lastSecondsCounter = fabs([lastStartTime timeIntervalSinceDate:[NSDate new]]);
+		if(lastSecondsCounter > ([connected floatValue] + 5)) {
+			//NSLog(@"flowReportSeconds: Closing connection because we've guessed it's a new connection.");
+			[self markConnectionAsClosed];
+			activeConnection = [self newConnection:connected];	
+		} else {
+			//NSLog(@"flowReportSeconds: Resuming previous connection...");
+		}
+	}	
+	firstRunSinceStartup = NO;
+
+	[activeConnection setValue:connected forKey:@"connectedSeconds"];
+	[activeConnection setValue:sent forKey:@"sentData"];
+	[activeConnection setValue:received forKey:@"recvData"];
+	[activeConnection setValue:[NSDate new] forKey:@"lastSeen"];
+	
+	[self calculateTotalUsage];
+	
+	if(cheezburgerWatch == nil) {
+		cheezburgerWatch = [NSTimer scheduledTimerWithTimeInterval:10 target:self selector:@selector(iCanHasCheezburger:) userInfo:nil repeats:YES];
+	}
+}
+
+-(NSManagedObject*)newConnection:(NSNumber*)connectedTime
+{
+	//NSLog(@"Creating new connection...");
+	activeConnection = [NSEntityDescription insertNewObjectForEntityForName:@"Connection" inManagedObjectContext:managedObjectContext];
+	[activeConnection setValue:connectedTime forKey:@"connectedSeconds"];
+	[activeConnection setValue:[NSNumber numberWithBool:NO] forKey:@"hasTerminated"];
+	[activeConnection setValue:[NSDate new] forKey:@"lastSeen"];
+	[activeConnection setValue:[NSDate new] forKey:@"startTime"];	
+	//NSLog(@"Done.");
+	[self calculateTotalUsageForCaching];
+	return activeConnection;
+}
+
+-(void)setupCoreData
+{
+	firstRunSinceStartup = YES;	
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+	NSString *thePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:[[NSBundle mainBundle]  objectForInfoDictionaryKey:@"CFBundleName"]];
+//	NSLog(@"Application support path is: %@", thePath);
+	
+	NSFileManager *man = [NSFileManager defaultManager];
+	if(![man fileExistsAtPath:thePath]) {
+		//NSLog(@"Path doesn't exist yet, creating it now...");
+		if(![man createDirectoryAtPath:thePath attributes:nil]) {
+			NSLog(@"AppSupport path still doesn't exist. Um. Oh oh?");
+		}
+	}
+		
+	NSURL *url = [NSURL fileURLWithPath:[thePath stringByAppendingPathComponent:@"ConnectionLog.db"]];
+	coreDataError = nil;
+	 
+	NSArray *bundles = [NSArray arrayWithObject:[NSBundle mainBundle]];
+	
+	model = [NSManagedObjectModel mergedModelFromBundles:bundles];
+	coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+	[coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:url options:nil error:&coreDataError];
+
+	if(coreDataError != nil) {
+		NSLog(@"setupCoreData addPersistentStoreWithType:etc... errored: %@", coreDataError);
+		coreDataError = nil;
+	}
+
+	managedObjectContext = [[NSManagedObjectContext alloc] init];
+	[managedObjectContext setPersistentStoreCoordinator: coordinator];
+	
+	[self calculateTotalUsageForCaching];
+}
+
+-(void)calculateTotalUsage
+{
+	SInt64 thisSent = 0, thisRecv = 0;
+	
+	if(activeConnection != nil) {
+		thisRecv = [[activeConnection valueForKey:@"recvData"] longLongValue];
+		thisSent = [[activeConnection valueForKey:@"sentData"] longLongValue];
+	}
+	
+	cachedTotalSent = (cachedSent + thisSent);
+	cachedTotalRecv = (cachedRecv + thisRecv);
+	
+	//NSLog(@"Totals: Sent %i, Received %i", cachedTotalSent, cachedTotalRecv);
+}
+
+-(SInt64)cachedTotalSent
+{
+	return cachedTotalSent;
+}
+-(SInt64)cachedTotalRecv
+{
+	return cachedTotalRecv;
+}
+
+-(void)calculateTotalUsageForCaching
+{
+	//NSLog(@"calculateTotalUsageForCaching: Begin...");
+	NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Connection" inManagedObjectContext:managedObjectContext];
+	NSFetchRequest *request = [[[NSFetchRequest alloc] init] autorelease];
+	[request setEntity:entityDescription];
+	
+	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"hasTerminated = 1"];
+	[request setPredicate:predicate];
+	
+	NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"startTime" ascending:NO];
+	[request setSortDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+	[sortDescriptor release];
+	
+	NSArray *array = [managedObjectContext executeFetchRequest:request error:&coreDataError];
+	if(coreDataError != nil || array == nil) {
+		NSLog(@"calculateTotalUsage executeFetchRequest errored: %@ (or array was nil)", coreDataError);
+		coreDataError = nil;
+	}
+	if ([array count] == 0) {	
+		return;
+	}
+	
+	cachedSent = 0;
+	cachedRecv = 0;
+	int results = [array count], i = 0;
+	
+	// [activeConnection valueForKey:@"startTime"]
+	for(i = 0; i < results; i++) {
+		cachedRecv = cachedRecv + [[[array objectAtIndex:i] valueForKey:@"recvData"] longLongValue];
+		cachedSent = cachedSent + [[[array objectAtIndex:i] valueForKey:@"sentData"] longLongValue];
+	}
+	
+	NSLog(@"Caching totals: Sent %lld, Received %lld", cachedSent, cachedRecv);
+}
+
+-(void)clearHistory
+{
+
+	NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Connection" inManagedObjectContext:managedObjectContext];
+	NSFetchRequest *request = [[[NSFetchRequest alloc] init] autorelease];
+	[request setEntity:entityDescription];
+	
+	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"hasTerminated = 1"];
+	[request setPredicate:predicate];
+	
+//	NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"startTime" ascending:NO];
+//	[request setSortDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+//	[sortDescriptor release];
+	
+	NSArray *array = [managedObjectContext executeFetchRequest:request error:&coreDataError];
+	if(coreDataError != nil || array == nil) {
+		NSLog(@"doWeHaveAnUnclosedConnection executeFetchRequest errored: %@ (or array was nil)", coreDataError);
+		coreDataError = nil;
+		return;
+	}
+	
+	NSEnumerator *enumerator;
+	NSManagedObject *obj;
+
+	enumerator = [array objectEnumerator];
+	while((obj = [enumerator nextObject]) != nil)
+	{
+		NSLog(@"should delete %@", obj);
+		[managedObjectContext deleteObject:obj];
+	}
+	
+	[managedObjectContext save:&coreDataError];
+	if(coreDataError != nil) {
+		NSLog(@"coreDataError on delete: %@", coreDataError);
+	}
+
+	[self calculateTotalUsageForCaching];
+	[self calculateTotalUsage];
+
+}
+
+-(NSManagedObject*)doWeHaveAnUnclosedConnection
+{
+	NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Connection" inManagedObjectContext:managedObjectContext];
+	NSFetchRequest *request = [[[NSFetchRequest alloc] init] autorelease];
+	[request setEntity:entityDescription];
+	
+	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"hasTerminated = 0"];
+	[request setPredicate:predicate];
+	
+	NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"startTime" ascending:NO];
+	[request setSortDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+	[sortDescriptor release];
+	
+	NSArray *array = [managedObjectContext executeFetchRequest:request error:&coreDataError];
+	if(coreDataError != nil || array == nil) {
+		NSLog(@"doWeHaveAnUnclosedConnection executeFetchRequest errored: %@ (or array was nil)", coreDataError);
+		coreDataError = nil;
+	}
+	if ([array count] == 0) {	
+		return false;
+	}
+
+	activeConnection = [array objectAtIndex:0];
+	return [array objectAtIndex:0];
+}
+
+-(void)markConnectionAsClosed
+{
+	if(activeConnection == nil)
+		return; // no connection.. duh.
+
+	NSLog(@"Marking connection as closed...");
+	
+	[activeConnection setValue:[NSNumber numberWithBool:YES] forKey:@"hasTerminated"];
+	[managedObjectContext save:&coreDataError];
+	if(coreDataError != nil) {
+		NSLog(@"Error in closing last connection: %@", coreDataError);
+		coreDataError = nil;
+	}
+		
+	[activeConnection release];
+	activeConnection = nil;
+	NSLog(@"Done.");
+	NSLog(@"mOC: %@", managedObjectContext);
+	[managedObjectContext save:&coreDataError];
+	if(coreDataError != nil) {
+		NSLog(@"markConnectionAsClosed save error: %@", coreDataError);
+	}
+	
+	[self calculateTotalUsageForCaching];
+}
+
+@end
---
+++ tags/rel1_2_1/StyledWindow.m
@@ -0,0 +1,229 @@
+//
+//  StyledWindow.m
+//  StyledWindow
+//
+//  Created by Jeff Ganyard on 11/3/06.
+/*
+	Copyright (c) 2006 Bithaus.
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+	Sending an email to ganyard (at) bithaus.com informing where the code is being used would be appreciated.
+*/
+
+/* large portions borrowed from Matt Gemmell's TunesWindow sample wth permission */
+
+#import "StyledWindow.h"
+#import "CTGradient.h"
+
+
+@implementation StyledWindow
+
+- (id)initWithContentRect:(NSRect)contentRect styleMask:(unsigned int)styleMask backing:(NSBackingStoreType)bufferingType defer:(BOOL)flag;
+{
+	unsigned int newStyle;
+	if (styleMask & NSTexturedBackgroundWindowMask)
+		newStyle = styleMask;
+	else
+		newStyle = (NSTexturedBackgroundWindowMask | styleMask);
+	
+	if (self = [super initWithContentRect:contentRect styleMask:newStyle backing:bufferingType defer:flag]) {
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidResize:) name:NSWindowDidResizeNotification object:self];
+		return self;
+	}
+	return nil;
+}
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4
+- (void)setToolbar:(NSToolbar *)toolbar
+{
+	// Only actually call this if we respond to it on this machine
+	if ([toolbar respondsToSelector:@selector(setShowsBaselineSeparator:)])
+		[toolbar setShowsBaselineSeparator:NO];
+
+	[super setToolbar:toolbar];
+}
+#endif
+
+
+- (void)dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowDidResizeNotification object:self];
+	
+	[borderStartColor release];
+	[borderEndColor release];
+	[borderEdgeColor release];
+	
+	[super dealloc];
+}
+
+- (void)windowDidResize:(NSNotification *)aNotification
+{
+	[self setBackgroundColor:[self styledBackground]];
+	if ([self forceDisplay])
+		[self display];
+}
+
+- (void)setMinSize:(NSSize)aSize
+{
+	[super setMinSize:NSMakeSize(MAX(aSize.width, 150.0), MAX(aSize.height, 150.0))];
+}
+
+- (void)setFrame:(NSRect)frameRect display:(BOOL)displayFlag animate:(BOOL)animationFlag
+{
+	[self setForceDisplay:YES];
+	[super setFrame:frameRect display:displayFlag animate:animationFlag];
+	[self setForceDisplay:NO];
+}
+
+- (NSColor *)styledBackground
+{
+	NSImage *bg = [[NSImage alloc] initWithSize:[self frame].size];
+	CTGradient *styledGradient;
+	if (![self borderStartColor] || ![self borderEndColor])
+		styledGradient = [CTGradient unifiedPressedGradient];
+	else
+		styledGradient = [CTGradient gradientWithBeginningColor:[self borderStartColor] endingColor:[self borderEndColor]];
+
+	// Set min width of temporary pattern image to prevent flickering at small widths
+	float minWidth = 300.0;
+	
+	// Create temporary image for top gradient
+	NSImage *topImg = [[NSImage alloc] initWithSize:NSMakeSize(MAX(minWidth, [self frame].size.width), [self topBorder]+1.0)];
+	[topImg lockFocus];
+	[styledGradient fillRect:NSMakeRect(0, 1, [topImg size].width, [topImg size].height) angle:270.0];
+	[topImg unlockFocus];
+	
+	// Create temporary image for bottom gradient
+	NSImage *bottomImg = [[NSImage alloc] initWithSize:NSMakeSize(MAX(minWidth, [self frame].size.width), [self bottomBorder]+1.0)];
+	[bottomImg lockFocus];
+	[styledGradient fillRect:NSMakeRect(0, 0, [bottomImg size].width, [bottomImg size].height-1.0) angle:270.0];
+	[bottomImg unlockFocus];
+	
+	// Begin drawing into our main image
+	[bg lockFocus];
+	
+	// Composite current background color into bg
+	[bgColor set];
+	//[[NSColor whiteColor] set];
+	NSRectFill(NSMakeRect(0, 0, [bg size].width, [bg size].height));
+	
+	// Composite bottom gradient
+	[bottomImg drawInRect:NSMakeRect(0, 0, [bg size].width, [self bottomBorder]) 
+				 fromRect:NSMakeRect(0, 0, [bg size].width, [self bottomBorder]) 
+				operation:NSCompositeSourceOver 
+				 fraction:1.0];
+	[bottomImg release];
+
+	// Composite top gradient
+	[topImg drawInRect:NSMakeRect(0, [bg size].height - [self topBorder], [bg size].width, [self topBorder]) 
+			  fromRect:NSMakeRect(0, 0, [bg size].width, [self topBorder]) 
+			 operation:NSCompositeSourceOver 
+			  fraction:1.0];
+	[topImg release];
+
+	// draw border edges
+	if (![self borderEdgeColor])
+		[[NSColor colorWithDeviceWhite:0.25 alpha:1.0] setFill];
+	else
+		[[self borderEdgeColor] setFill];		
+
+	NSRectFill(NSMakeRect(0, [bg size].height - [self topBorder], [bg size].width, 1.0));
+	NSRectFill(NSMakeRect(0, [self bottomBorder], [bg size].width, 1.0));
+	
+	[bg unlockFocus];
+	
+	return [NSColor colorWithPatternImage:[bg autorelease]];
+}
+
+
+- (BOOL)forceDisplay
+{
+	return forceDisplay;
+}
+- (void)setForceDisplay:(BOOL)flag
+{
+	forceDisplay = flag;
+}
+
+
+- (float)topBorder
+{
+	return topBorder;
+}
+- (void)setTopBorder:(float)newTopBorder
+{
+	topBorder = newTopBorder;
+}
+
+
+- (float)bottomBorder
+{
+	return bottomBorder;
+}
+- (void)setBottomBorder:(float)newBottomBorder
+{
+	bottomBorder = newBottomBorder;
+}
+
+
+- (NSColor *)borderStartColor
+{
+	return borderStartColor; 
+}
+- (void)setBorderStartColor:(NSColor *)newBorderStartColor
+{
+	if (borderStartColor != newBorderStartColor) {
+		[newBorderStartColor retain];
+		[borderStartColor release];
+		borderStartColor = newBorderStartColor;
+	}
+}
+
+
+- (NSColor *)borderEndColor
+{
+	return borderEndColor; 
+}
+- (void)setBorderEndColor:(NSColor *)newBorderEndColor
+{
+	if (borderEndColor != newBorderEndColor) {
+		[newBorderEndColor retain];
+		[borderEndColor release];
+		borderEndColor = newBorderEndColor;
+	}
+}
+
+
+- (NSColor *)borderEdgeColor
+{
+	return borderEdgeColor; 
+}
+- (void)setBorderEdgeColor:(NSColor *)newBorderEdgeColor
+{
+	if (borderEdgeColor != newBorderEdgeColor) {
+		[newBorderEdgeColor retain];
+		[borderEdgeColor release];
+		borderEdgeColor = newBorderEdgeColor;
+	}
+}
+
+
+- (NSColor *)bgColor
+{
+	return bgColor; 
+}
+- (void)setBgColor:(NSColor *)newBgColor
+{
+	if (bgColor != newBgColor) {
+		[newBgColor retain];
+		[bgColor release];
+		bgColor = newBgColor;
+	}
+}
+
+@end
